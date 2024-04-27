0019681f5248d98d93be6630ae44cdc53c35d93f
hanyas <hanyas@users.noreply.github.com>
2024-04-27 16:55:38 +0300

    update burgers scripts

--- examples/burgers.py
+++ examples/burgers.py
@@ -3,15 +3,13 @@ from typing import Callable, NamedTuple
 
 import jax
 import jax.numpy as jnp
-import matplotlib.pyplot as plt
+
 from parsmooth._base import FunctionalModel, MVNStandard
 from parsmooth.linearization import extended
 from parsmooth.methods import filter_smoother, iterated_smoothing
 
-jax.config.update("jax_enable_x64", True)
-# jax.config.update("jax_platform_name", "cuda")
-# jax.config.update("jax_debug_nans", True)
-# jax.config.update("jax_disable_jit", True)
+import time
+import matplotlib.pyplot as plt
 
 
 class PDE(NamedTuple):
@@ -29,9 +27,9 @@ class PDE(NamedTuple):
 def construct_grid(pde: PDE):
     J = int((pde.b - pde.a) / pde.dx)
     N = int(pde.T / pde.dt)
-    x = jnp.linspace(pde.a, pde.b, J + 1)
-    t = jnp.linspace(0.0, pde.T, N + 1)
-    return x, t
+    x_grid = jnp.linspace(pde.a, pde.b, J + 1)
+    t_grid = jnp.linspace(0.0, pde.T, N + 1)
+    return x_grid, t_grid
 
 
 @jax.jit
@@ -45,106 +43,197 @@ def construct_gram_matrix(x: jax.Array, kernel_fn: Callable, *args):
 
 
 def get_transition_model(
-    pde: PDE, x: jax.Array, q: float = 10.0, sigma: float = 10.0, nell: float = 2.0
+    pde: PDE, x_grid: jax.Array, q: float = 0.5,
+    sigma: float = 25, nell: float = 2.0
 ):
     dt = pde.dt
     dx = pde.dx
 
     # IWP-1 prior.
-    A_dt = jnp.array([[1, dt], [0, 1]])
-    Q_dt = q * jnp.array([[dt**3 / 3, dt**2 / 2], [dt**2 / 2, dt]])
+    A_dt = jnp.array(
+        [
+            [1, dt],
+            [0, 1]
+        ]
+    )
+    Q_dt = q * jnp.array(
+        [
+            [dt**3 / 3, dt**2 / 2],
+            [dt**2 / 2, dt]
+        ]
+    )
 
-    J = len(x) - 1
-    I_Jm1 = jnp.eye(J - 1)
     ell = nell * dx
-    C_Jm1 = construct_gram_matrix(x[1:-1], squared_exponential_kernel, sigma, ell)
+    C_Jm1 = construct_gram_matrix(x_grid[1:-1], squared_exponential_kernel, sigma, ell)
+
+    J = len(x_grid) - 1
+    I_Jm1 = jnp.eye(J - 1)
 
     A = jnp.kron(A_dt, I_Jm1)
     Q = jnp.kron(Q_dt, C_Jm1)
-    return FunctionalModel(lambda u: A @ u, MVNStandard(jnp.zeros(Q.shape[0]), Q))
+    return FunctionalModel(
+        lambda u: A @ u,
+        MVNStandard(jnp.zeros(Q.shape[0]), Q)
+    )
 
 
-def pseudo_observation_fn(pde: PDE, x: jax.Array):
-    """This is specific for Burgers' equation."""
-    J = int(len(x) / 2) + 1
-    u = x[: J - 1]
+def burgers_equation_fn(pde: PDE, s: jax.Array):
+    J = int(len(s) / 2) + 1
+    u = s[:J - 1]
     u_jp1 = jnp.concat((u[1:], jnp.array([pde.u_b])))
     u_jm1 = jnp.concat((jnp.array([pde.u_a]), u[:-1]))
     f = (pde.flux(u_jp1) - pde.flux(u_jm1)) / (2.0 * pde.dx)
-    du = x[J - 1 :]
+    du = s[J - 1:]
     return du + f
 
 
-def solve_pde(pde: PDE, n_iter: int = 1):
-    x, t = construct_grid(pde)
-    J = len(x) - 1
-    N = len(t) - 1
+def flux(u):
+    return 0.5 * u**2
 
-    # Specify the initial mean and covariance.
-    us = pde.u_0(x[1:-1])
-    dus = jnp.zeros(J - 1)
-    m0 = jnp.concat((us, dus))
-    P0 = jax.scipy.linalg.block_diag(jnp.zeros((J - 1, J - 1)), jnp.eye(J - 1))
-    s0 = MVNStandard(m0, P0)
 
-    # Get the transition and observation models.
-    transition_model = get_transition_model(pde, x)
-    observation_model = FunctionalModel(
-        lambda s: pseudo_observation_fn(pde, s), MVNStandard(jnp.zeros(J - 1), 0.0)
-    )
-    observations = jnp.zeros((N, J - 1))
-    init_nominal_trajectory = filter_smoother(
-        observations, s0, transition_model, observation_model, extended, None, False
-    )
-    smoothed_trajectory = iterated_smoothing(
-        observations,
-        s0,
-        transition_model,
-        observation_model,
-        extended,
-        init_nominal_trajectory,
-        criterion=lambda i, *_: i < n_iter + 1,
-        parallel=True,
-        return_loglikelihood=False,
-    )
-    return t, x, smoothed_trajectory
+def u_0(u):
+    return -jnp.sin(jnp.pi * u)
 
 
 if __name__ == "__main__":
 
-    def flux(u):
-        return 0.5 * u**2
+    jax.config.update("jax_enable_x64", True)
+    jax.config.update("jax_platform_name", "cpu")
 
-    def u_0(u):
-        return -jnp.sin(jnp.pi * u)
+    max_t = 1.0
+    min_x = -1.0
+    max_x = 1.0
 
     dx = 0.01
     dt = 0.01
-    burgers = PDE(-1.0, 1.0, 0.0, 0.0, u_0, 1.5, dx, dt, flux)
-    t, x, smoothed_trajectory = solve_pde(burgers)
 
-    # Get the posterior mean and variance of the solution.
-    J = len(x) - 1
-    H_s = jnp.kron(jnp.array([1.0, 0.0]), jnp.eye(J - 1))
-    us = jnp.einsum("ij, tj -> ti", H_s, smoothed_trajectory.mean)
-    Ps = jnp.einsum("ij, tjk, kl -> til", H_s, smoothed_trajectory.cov, H_s.T)
-    Ps = jnp.diagonal(Ps, axis1=1, axis2=2)
+    pde = PDE(min_x, max_x, 0.0, 0.0, u_0, max_t, dx, dt, flux)
 
-    # Plotting code from Simo.
-    # Frame have to be adjusted according to the discretization.
-    ps_frames = [0, 50, 100, 150]
+    x_grid, t_grid = construct_grid(pde)
+    x_size = len(x_grid) - 1
+    t_size = len(t_grid) - 1
+
+    us = pde.u_0(x_grid[1:-1])
+    dus = jnp.zeros(x_size - 1)
+
+    m0 = jnp.concat((us, dus))
+    P0 = 1e-2 * jax.scipy.linalg.block_diag(
+        jnp.eye(x_size - 1) * 0.0,
+        jnp.eye(x_size - 1),
+    )
+    prior = MVNStandard(m0, P0)
+
+    transition_model = get_transition_model(pde, x_grid)
+
+    observation_model = FunctionalModel(
+        lambda s: burgers_equation_fn(pde, s),
+        MVNStandard(jnp.zeros(x_size - 1), jnp.eye(x_size - 1) * 1e-8)
+    )
+
+    observations = jnp.zeros((t_size, x_size - 1))
+
+    init_nominal_trajectory = filter_smoother(
+        observations, prior, transition_model, observation_model, extended, None, False
+    )
+
+    nb_iter = 10
+
+    @jax.jit
+    def par_solver(init_trajectory, nb_iter):
+        par_res = iterated_smoothing(
+            observations,
+            prior,
+            transition_model,
+            observation_model,
+            extended,
+            init_trajectory,
+            parallel=True,
+            criterion=lambda i, *_: i < nb_iter,
+            return_loglikelihood=False,
+        )
+        return par_res
+
+
+    @jax.jit
+    def seq_solver(init_trajectory, nb_iter):
+        seq_res = iterated_smoothing(
+            observations,
+            prior,
+            transition_model,
+            observation_model,
+            extended,
+            init_trajectory,
+            parallel=False,
+            criterion=lambda i, *_: i < nb_iter,
+            return_loglikelihood=False,
+        )
+        return seq_res
+
+
+    smoothed_trajectory = par_solver(init_nominal_trajectory, nb_iter)
+    jax.block_until_ready(smoothed_trajectory)
+
+    start = time.time()
+    smoothed_trajectory = par_solver(init_nominal_trajectory, nb_iter)
+    jax.block_until_ready(smoothed_trajectory)
+    end = time.time()
+    print("time: ", end - start)
+
+    # Get the posterior mean and variance of the solution
+    mask = jnp.kron(jnp.array([1.0, 0.0]), jnp.eye(x_size - 1))
+    us_par = jnp.einsum("ij, tj -> ti", mask, smoothed_trajectory.mean)
+    Ps_par = jnp.einsum("ij, tjk, kl -> til", mask, smoothed_trajectory.cov, mask.T)
+    Ps_par = jnp.diagonal(Ps_par, axis1=1, axis2=2)
+
+    # Frame have to be adjusted according to the discretization
+    ieks_frames = [0, int(max_t / dt / 3), int(2 * max_t / dt / 3), int(max_t / dt)]
+
     fig = plt.figure(figsize=(12, 9))
-    for idx, frame_idx in enumerate(ps_frames):
+    for idx, frame_idx in enumerate(ieks_frames):
         ax = fig.add_subplot(1, 4, idx + 1)
         ax.fill_between(
-            x[1:-1],
-            us[frame_idx, :] - 1.96 * jnp.sqrt(Ps[frame_idx, :]),
-            us[frame_idx, :] + 1.96 * jnp.sqrt(Ps[frame_idx, :]),
+            x_grid[1:-1],
+            us_par[frame_idx, :] - 3.0 * jnp.sqrt(Ps_par[frame_idx, :]),
+            us_par[frame_idx, :] + 3.0 * jnp.sqrt(Ps_par[frame_idx, :]),
             label="Confidence",
         )
-        ax.plot(x[1:-1], us[frame_idx, :], "r-", linewidth=2.0, label="IEKS")
+        ax.plot(x_grid[1:-1], us_par[frame_idx, :], "r-", linewidth=2.0, label="IEKS")
         if idx == 0:
             ax.set_ylabel("$u$", fontsize="large")
-        ax.set_title(r"time = {:.2f}".format(t[frame_idx + 1]), fontsize="large")
+        ax.set_title(r"time = {:.2f}".format(t_grid[frame_idx]), fontsize="large")
+        ax.legend(fontsize="large", loc="upper right")
+    plt.show()
+
+    smoothed_trajectory = seq_solver(init_nominal_trajectory, nb_iter)
+    jax.block_until_ready(smoothed_trajectory)
+
+    start = time.time()
+    smoothed_trajectory = seq_solver(init_nominal_trajectory, nb_iter)
+    jax.block_until_ready(smoothed_trajectory)
+    end = time.time()
+    print("time: ", end - start)
+
+    # Get the posterior mean and variance of the solution
+    mask = jnp.kron(jnp.array([1.0, 0.0]), jnp.eye(x_size - 1))
+    us_seq = jnp.einsum("ij, tj -> ti", mask, smoothed_trajectory.mean)
+    Ps_seq = jnp.einsum("ij, tjk, kl -> til", mask, smoothed_trajectory.cov, mask.T)
+    Ps_seq = jnp.diagonal(Ps_seq, axis1=1, axis2=2)
+
+    # Frame have to be adjusted according to the discretization
+    ieks_frames = [0, int(max_t / dt / 3), int(2 * max_t / dt / 3), int(max_t / dt)]
+
+    fig = plt.figure(figsize=(12, 9))
+    for idx, frame_idx in enumerate(ieks_frames):
+        ax = fig.add_subplot(1, 4, idx + 1)
+        ax.fill_between(
+            x_grid[1:-1],
+            us_seq[frame_idx, :] - 3.0 * jnp.sqrt(Ps_seq[frame_idx, :]),
+            us_seq[frame_idx, :] + 3.0 * jnp.sqrt(Ps_seq[frame_idx, :]),
+            label="Confidence",
+        )
+        ax.plot(x_grid[1:-1], us_seq[frame_idx, :], "r-", linewidth=2.0, label="IEKS")
+        if idx == 0:
+            ax.set_ylabel("$u$", fontsize="large")
+        ax.set_title(r"time = {:.2f}".format(t_grid[frame_idx]), fontsize="large")
         ax.legend(fontsize="large", loc="upper right")
     plt.show()
--- examples/test_burgers.py
+++ examples/test_burgers.py
@@ -1,10 +1,11 @@
 import jax
 import jax.numpy as jnp
+
 from burgers import (
     PDE,
     construct_gram_matrix,
     construct_grid,
-    pseudo_observation_fn,
+    burgers_equation_fn,
     squared_exponential_kernel,
 )
 
@@ -20,17 +21,16 @@ def test_construct_gram_matrix():
     assert C[3, 2] == kernel_fn(3.0, 2.0, sigma, ell)
 
 
-def test_pseudo_observation_fn_derivative():
-    """Test the Jacobian of the pseudo-observation function."""
+def test_burgers_equation_fn_derivative():
     flux = lambda u: u**2 / 2
     u_0 = lambda u: -jnp.sin(jnp.pi * u)
     burgers = PDE(-1.0, 1.0, 0.0, 0.0, u_0, 1.5, 0.1, 0.05, flux)
-    x, _ = construct_grid(burgers)
-    J = len(x) - 1
-    us = burgers.u_0(x[1:-1])
+    x_grid, _ = construct_grid(burgers)
+    J = len(x_grid) - 1
+    us = burgers.u_0(x_grid[1:-1])
     dus = jnp.zeros(J - 1)
     m0 = jnp.concat((us, dus))
-    jac = jax.jacfwd(pseudo_observation_fn, argnums=1)(burgers, m0)
-    jac = jac[:, : J - 1]
-    assert jnp.allclose(jnp.diagonal(jac, offset=-1), -m0[: J - 2] / (2.0 * burgers.dx))
-    assert jnp.allclose(jnp.diagonal(jac, offset=1), m0[1 : J - 1] / (2.0 * burgers.dx))
+    jac = jax.jacfwd(burgers_equation_fn, argnums=1)(burgers, m0)
+    jac = jac[:, :J - 1]
+    assert jnp.allclose(jnp.diagonal(jac, offset=-1), - m0[: J - 2] / (2.0 * burgers.dx))
+    assert jnp.allclose(jnp.diagonal(jac, offset=1), m0[1: J - 1] / (2.0 * burgers.dx))
