9c7e9297f48c82ab70187d23868731044a092967
hanyas <hanyas@users.noreply.github.com>
2024-04-27 22:03:45 +0300

    update allen-cahn

--- examples/allen_cahn_high_fidelity.py
+++ examples/allen_cahn_high_fidelity.py
@@ -9,11 +9,11 @@ import numpy as np
 from pde import PDE, CartesianGrid, MemoryStorage, ScalarField, plot_kymograph
 
 
-def run_high_fidelity(dt, dx, max_t, a, b):
+def run_high_fidelity(dt, dx, max_t, min_x, max_x):
 
     # initialize the equation and the space
     eq = PDE({"φ": "0.0001*laplace(φ) + 5 * φ - 5 * φ**3"})
-    grid = CartesianGrid([[a, b]], [int((b-a)/dx)], periodic=True)
+    grid = CartesianGrid([[min_x, max_x]], [int((max_x - min_x) / dx)], periodic=True)
     state = ScalarField.from_expression(grid, "x**2 * cos(pi*x)")
     
     # solve the equation and store the trajectory
@@ -21,6 +21,6 @@ def run_high_fidelity(dt, dx, max_t, a, b):
     eq.solve(state, t_range=max_t, solver="scipy", tracker=storage.tracker(dt))
     
     t = np.linspace(0, max_t, int(max_t / dt))
-    x = np.linspace(a, b, int((b - a) / dx))
+    x = np.linspace(min_x, max_x, int((max_x - min_x) / dx))
     
     return t, x, storage.data
