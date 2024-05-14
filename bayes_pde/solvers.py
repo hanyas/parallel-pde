511b96d8332f13b7dfffb07ec5af10a3a18dfc1c
hanyas <hanyas@users.noreply.github.com>
2024-05-14 18:42:53 +0300

    add solvers from second order smoother package

--- bayes_pde/solvers.py
+++ bayes_pde/solvers.py
@@ -1,6 +1,9 @@
-from parsmooth.linearization import extended, cubature
+from parsmooth.linearization import extended
 from parsmooth.methods import iterated_smoothing
-from newton_smoothers import iterated_batch_gauss_newton_smoother
+
+from newton_smoothers import trust_region_iterated_batch_gauss_newton_smoother
+from newton_smoothers import trust_region_iterated_recursive_gauss_newton_smoother
+from newton_smoothers import trust_region_iterated_recursive_newton_smoother
 
 
 def batch_solver(
@@ -11,13 +14,13 @@ def batch_solver(
     init_trajectory,
     nb_iter=10,
 ):
-    return iterated_batch_gauss_newton_smoother(
+    return trust_region_iterated_batch_gauss_newton_smoother(
         init_trajectory.mean,
         observations,
         prior,
         transition_model,
         observation_model,
-        nb_iter=10,
+        nb_iter=nb_iter,
     )[0]
 
 
@@ -42,6 +45,46 @@ def sequential_solver(
         criterion=lambda i, *_: i < nb_iter,
         return_loglikelihood=return_loglikelihood,
     )
+
+
+def sequential_solver_with_trust_region(
+    observations,
+    prior,
+    transition_model,
+    observation_model,
+    init_trajectory,
+    linearization_method,
+    nb_iter=10,
+):
+    return trust_region_iterated_recursive_gauss_newton_smoother(
+        init_trajectory,
+        observations,
+        prior,
+        transition_model,
+        observation_model,
+        linearization_method,
+        nb_iter=nb_iter,
+    )[0]
+
+
+def second_order_sequential_solver_with_trust_region(
+    observations,
+    prior,
+    transition_model,
+    observation_model,
+    init_trajectory,
+    linearization_method,
+    nb_iter=10,
+):
+    return trust_region_iterated_recursive_newton_smoother(
+        init_trajectory,
+        observations,
+        prior,
+        transition_model,
+        observation_model,
+        linearization_method,
+        nb_iter=nb_iter,
+    )[0]
 
 
 def parallel_solver(
